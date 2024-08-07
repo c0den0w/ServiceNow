1c29ea567e279fc3a0ab37f49dae93977a35ae86
c0den0w <yupredtenteen@proton.me>
2024-08-07 20:56:36 +0530

    add comments to SelectAllFromContextMenu

--- SelectAllFromContextMenu/ContextMenuUtils.js
+++ SelectAllFromContextMenu/ContextMenuUtils.js
@@ -1,3 +1,4 @@
+//Client callable script include
 var ContextMenuUtils = Class.create();
 ContextMenuUtils.prototype = Object.extendsObject(AbstractAjaxProcessor, {
     getValues: function() {
@@ -6,7 +7,9 @@ ContextMenuUtils.prototype = Object.extendsObject(AbstractAjaxProcessor, {
         var encodedQuery = this.getParameter("sysparm_encoded_query");
         var resultArray = [];
         var gr = new GlideRecord(table);
-        var count = 0;
+        var count = 0; //this variable counts the number of rows returned
+
+        //if condition in case a filter condition is added to the list
         if (encodedQuery) {
             gr.addEncodedQuery(encodedQuery);
             gr.query();
@@ -14,21 +17,15 @@ ContextMenuUtils.prototype = Object.extendsObject(AbstractAjaxProcessor, {
             gr.query();
         }
         while (gr.next()) {
-            count += 1;
+            count += 1;     //counting
             if (gr.getValue(field)) {
-                resultArray.push(gr.getValue(field));
+                resultArray.push(gr.getValue(field));   //gets the value of the selected field and pushes it to an array declared earlier
             }
         }
         if (resultArray) return JSON.stringify({
             "result": resultArray,
             "rows": count
         });
-    },
-
-    displayInfoMessage: function() {
-        gs.addInfoMessage("Copied");
-        g_form.addInfoMessage("copied");
-        return "";
     },
     type: 'ContextMenuUtils'
 });
--- SelectAllFromContextMenu/SelectAllFromContextMenu.js
+++ SelectAllFromContextMenu/SelectAllFromContextMenu.js
@@ -1,26 +1,22 @@
 runAction();
 
 function runAction() {
-    var ga = new GlideAjax("ContextMenuUtils");
-    ga.addParam("sysparm_name", "getValues");
-    ga.addParam("sysparm_table_name", g_list.getTableName());
-    ga.addParam("sysparm_field_name", g_fieldName);
-    ga.addParam("sysparm_encoded_query", g_list.getQuery());
-    ga.getXMLAnswer(callback);
+    var ga = new GlideAjax("ContextMenuUtils");   //Creating an instance of the script include and calling the method
+    ga.addParam("sysparm_name", "getValues");     //name of the method to call
+    ga.addParam("sysparm_table_name", g_list.getTableName());  //required parameter
+    ga.addParam("sysparm_field_name", g_fieldName);    //required parameter
+    ga.addParam("sysparm_encoded_query", g_list.getQuery());     //required parameter
+    ga.getXMLAnswer(callback);    //Makes an asynchronous call to the server. On completion callback is invoked
 
-
+    //callback function will have a response argument of type any 
     function callback(response) {
         var res = JSON.parse(response);
 
-        var parsedRes = res.result.join("\n");
-        navigator.clipboard.writeText(parsedRes);
-
+        var parsedRes = res.result.join("\n");  //converts array to a string separated by a newline character (for copying)
+        navigator.clipboard.writeText(parsedRes);   //native JS browser method to copy to clipboard
         if (response) {
-            var gm = new GlideModal("CopiedToClipboard");
-            //Sets the dialog title
-            gm.setTitle('Info');
-            gm.setWidth(550);
-            gm.renderWithContent("<div style='font-size:16px;font-weight:600;color:#3388dd;'>Copied " + res.rows + " value(s) </div>");
+            //alert to notify the user that values are copied
+            alert(res.rows + " value(s) copied to clipboard");           
         }
     }
 }
